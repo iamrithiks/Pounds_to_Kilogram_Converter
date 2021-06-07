2e3ce9a7a4fa2b26627b9470c291e2f246512a37
Rithik Samanthula <talk2rithiks@gmail.com>
2021-06-07 11:03:36 -0400

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double numberOfPounds = 1d;
+        double convertedKilograms = numberOfPounds * 0.45359237D;
+        System.out.println("Converted Kilograms = " + convertedKilograms);
+    }
+}
